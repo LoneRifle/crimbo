52ff23a33c2530b559f0082e75964c53bbd6f917
LoneRifle <alwyn.tan@gmail.com>
2024-03-02 10:46:54 +0000

    feat: use flexsearch to match requests against offers

--- postprocess.js
+++ postprocess.js
@@ -1,15 +1,38 @@
 const fs = require('node:fs')
 
-async function go() {
-  // Read the downloaded_filename JSON
-  const requests = JSON.parse(fs.readFileSync('requests.json'))
-  const offers = JSON.parse(fs.readFileSync('offers.json'))
+const { Document } = require('flexsearch')
 
+const requests = JSON.parse(fs.readFileSync('requests.json'))
+const offers = JSON.parse(fs.readFileSync('offers.json'))
 
-  // fs.writeFileSync(process.env.POSTPROCESS_FILENAME || 'matches.json',  JSON.stringify(matches, null, 2))
+const document = new Document({
+  preset: 'match',
+  document: {
+    index: [
+      'name',
+    ],
+    store: true,
+  },
+})
+
+for (const offer of offers) {
+  document.add(offer)
 }
 
-go()
-  .then(() => 
-    console.log('Done.')
-  )
+const matches = []
+
+for (const request of requests) {
+  const result = document.search(request.name, { enrich: true })
+    .flatMap(({ result }) => result)
+  if (result.length) {
+    matches.push({
+      ...request,
+      matches: result,
+    })
+  }
+}
+
+fs.writeFileSync(
+  process.env.POSTPROCESS_FILENAME || 'matches.json',  
+  JSON.stringify(matches, null, 2)
+)
